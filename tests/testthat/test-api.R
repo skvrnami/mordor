f632237d0399ccb2fd64cffc0dc2757b572d9804
Michael Škvrňák <397925@mail.muni.cz>
2018-04-09 23:13:47 +0200

    Feature/tests (#3)
    * Add tests

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -25,6 +25,17 @@ test_that("API tag output returns error when source is wrong", {
     expect_error(tag_morphodita("A je to", source = "špatný"))
 })
 
+a <- tag_morphodita("A je to", source = "docker")
+b <- tag_morphodita("A je to", source = "lindat", tagset = "pdt_to_conll2009")
+test_that("Splitting tags returns data.frame with splitted POS tag", {
+    expect_equal(ncol(split_tags(a)$output), 17)
+    expect_equal(ncol(split_tags(b)$output), 17)
+})
+
+test_that("Recoding tags returns data.frame", {
+    expect_equal(ncol(split_tags(a)$output), ncol(recode_tags(split_tags(a))$output))
+})
+
 context("Test generation")
 get_salutation <- function(data){
     gen_morphodita(data) %>%
